5dc918f367871357034b60509cfac4b2379ddde2
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-08-31 10:38:15 +0600

    infitie scroll added

--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -5,28 +5,215 @@ const Topbar = () => {
     // eslint-disable-next-line jsx-a11y/no-distracting-elements
     <marquee
       behavior=""
+      id="my-marquee"
       direction=""
       className="py-2 bg-black mb-[-5px] relative"
     >
-      <div className=" text-[#00F902] px-4 h-[60px] flex justify-between items-center">
-        <a href="/" className="text-xl">
-          BUY BUY BUY BUY
-        </a>
-        <a href="/" className="text-xl">
-          TIKI PRICE: $0.09
-        </a>
-        <a href="/" className="text-xl">
-          REWARDS: BNB
-        </a>
-        <a href="/" className="text-xl">
-          BURNED: 5000
-        </a>
-        <a href="/" className="text-xl">
-          SUPPLY: 999000
-        </a>
-        <a href="/" className="text-xl">
-          BUY BUY BUY BUY
-        </a>
+      <div className="track flex justify-between items-center">
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
+        <div className=" text-[#00F902] px-4 h-[60px] flex gap-10 justify-between items-center">
+          <a href="/" className="text-xl">
+            BUY BUY BUY BUY
+          </a>
+          <a href="/" className="text-xl">
+            TIKI PRICE: $0.09
+          </a>
+          <a href="/" className="text-xl">
+            REWARDS: BNB
+          </a>
+          <a href="/" className="text-xl">
+            BURNED: 5000
+          </a>
+          <a href="/" className="text-xl">
+            SUPPLY: 999000
+          </a>
+        </div>
       </div>
     </marquee>
   );
